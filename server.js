c3bd49a3d421472ce29db6056687b12881138b50
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-09-29 02:01:30 +0300

    add delete

--- server.js
+++ server.js
@@ -2,17 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 var cors = require('cors')
-
 const helmet = require("helmet");
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-
   app.use(cors());
-
   app.use(helmet());
-
-app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
 const readFile = (filePath) => {
     try {
@@ -23,20 +20,34 @@ const readFile = (filePath) => {
       return [];
     }
   };
+
   const jsonFilePath = 'data.json';
   let data = readFile(jsonFilePath) || [];
 
-
 app.post('/kaydet', (req, res) => {
   const { lat, lng, datetime } = req.body;
-  const id = data.length; // ID 
-
+  const id = data.length; // ID
   const newData = { id, lat, lng, datetime };
   data.push(newData);
+  fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2));
+  res.json(newData);
+});
+
+app.post('/sil', (req, res) => {
+  const { id } = req.body;
+  const index = data.findIndex(item => item.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: 'Belirtilen ID ile öğe bulunamadı' });
+  }
+
+  data.splice(index, 1);
 
   fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2));
+  res.json({ message: 'Öğe başarıyla silindi' });
+});
 
-  res.json(newData);
+app.get('/veriler', (req, res) => {
+  res.json(data);
 });
 
 app.get('/indir', (req, res) => {
